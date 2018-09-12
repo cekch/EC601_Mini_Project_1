1488ee34786ad4c69b4bc55fffec88ad5cadd117
Caroline <cekchian@bu.edu>
2018-09-12 13:09:30 -0400

    Added function convert_images_to_video(). This function currently contains dummy print outs that describe the intended behavior of the function. A function call to convert_images_to_video() was added to main().

--- mini_project_1.py
+++ mini_project_1.py
@@ -10,11 +10,18 @@ def download_twitter_images():
     print('In function: download_twitter_images')
     print('Second function called.')
     print('This function is not yet implemented.')
-    print('It will download the images from a twitter feed.')
+    print('It will download the images from a twitter feed. \n')
+	
+def convert_images_to_video():
+    print('In function: convert_images_to_video')
+    print('Third function called.')
+    print('This function is not yet implemented.')
+    print('It will use ffmpeg to convert the images that were downloaded to a video.')
 	
 def main():
     authorize_twitter()
     download_twitter_images()
+    convert_images_to_video()
 	
 if __name__ == '__main__':
     main()
