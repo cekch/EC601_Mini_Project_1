1d37e03a7daa75ac07a6afa1959ed0b0644c72af
Caroline <cekchian@bu.edu>
2018-09-12 13:03:22 -0400

    Added function authorize_twitter() with dummy output that explains intended behavior of function. Added a main function definition that calls authorize_twitter().

---
+++ mini_project_1.py
@@ -0,0 +1,13 @@
+#Author: Caroline Ekchian, cekchian@bu.edu
+#import twitter API library: going to be using tweepy
+
+def authorize_twitter():
+    print('In function: authorize_twitter')
+    print('First function called.')
+    print('This function will authorize the usage of the tweepy.')
+	
+def main():
+    authorize_twitter()
+	
+if __name__ == '__main__':
+    main()
