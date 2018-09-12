e60d999f0cb752ced2858b11ee5e22a7767ed212
Caroline <cekchian@bu.edu>
2018-09-12 13:13:09 -0400

    Added function analyze_video(). This function currently contains dummy print outs that explain the intended behavior of the function. A function call to analyze_video() was also added to the main() function.

--- mini_project_1.py
+++ mini_project_1.py
@@ -16,12 +16,20 @@ def convert_images_to_video():
     print('In function: convert_images_to_video')
     print('Third function called.')
     print('This function is not yet implemented.')
-    print('It will use ffmpeg to convert the images that were downloaded to a video.')
+    print('It will use ffmpeg to convert the images that were downloaded to a video. \n')
+	
+def analyze_video():
+    print('In function: analyze_video')
+    print('Fourth function called.')
+    print('This function is not yet implemeted')
+    print('It will use the google vision api to describe the video of the images using text.')
+	
 	
 def main():
     authorize_twitter()
     download_twitter_images()
     convert_images_to_video()
+    analyze_video()
 	
 if __name__ == '__main__':
     main()
