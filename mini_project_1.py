bc6d00caeaae1ab8503fa246aebf897a6ff94b8a
Caroline <cekchian@bu.edu>
2018-09-17 22:29:29 -0400

    Removed keys.

--- mini_project_1.py
+++ mini_project_1.py
@@ -9,16 +9,10 @@ import os
 
 #google API key
 def authorize_twitter():
-    consumer_token='ckvs5fS5TUFSuZPzYYvFuqUrH'
-    consumer_secret='9csfakIHTpO0BSSJAnQICu5SB8nDqhC73JQXe39NXhpOLcZPgN'
-    access_key='355214663-PY1omoDd9MhhuM41GUpaJ2FOJNK2G5UVLtwauDdY'
-    access_secret='37oD1C45NSDXBYad3qOMtxU76Ynn2VkI4B0KLb6VYYKJt'
-    '''
     consumer_token="Put consumer API key here"
     consumer_secret="Put secret consumer API key here"
     access_key="Put access key here"
     access_secret="Put secret access key here"
-    '''
     authorization_handler=tweepy.OAuthHandler(consumer_token, consumer_secret)
     authorization_handler.set_access_token(access_key, access_secret)
     api=tweepy.API(authorization_handler)
